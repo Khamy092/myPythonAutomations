9996bb52e4243826a0e20ec8b9874a9c4910bfb9
khamy092 <khamy092@mymail.unisa.edu.au>
2023-01-17 09:00:58 +1030

    minor changes to the code.

--- EnrollPro.py
+++ EnrollPro.py
@@ -6,6 +6,7 @@
 
 from tqdm import tqdm
 import time
+
 # list of all the courses I need to enroll in:
 
 courseCodes = ['INFT 1016', 'COMP 1039', 'INFT 1012', 'INFT 1030', 'INFS 1025', 'INFS 1026', 'INFT 1031',
@@ -37,14 +38,14 @@ preRequisites = {
         "ICTPROJECT": 72
     }
 
-# Course Co-requisites:
+# Courses with Co-requisites:
 coRequisites = {
         "INFS 2045": "INFS 2044",
         "INFS 2043": "INFS 2041",
         "INFT 2064": "COMP 2012"
 }
 
-# Course study periods:
+# Courses in each study periods:
 study_periods = {
     2: ['INFT 1016', 'COMP 1039', 'INFT 1012', 'INFT 1030', 'INFS 1025', 'INFS 1026', 'INFT 1031',
             'COMP 1046', 'INFS 2044', 'INFS 2045', 'INFS 3090', 'INFS 4020', 'COMP 2035', 'COMP 2012',
@@ -81,20 +82,25 @@ def checkCoRequisites(course): # function to check if the co-requisites are met
         return True
 
 def checkStudyPeriod(course): # function to check if the course is available in the study period
+
     study_period = int(input("Enter the study period (2 or 5): "))
+
     while study_period != 2 and study_period != 5:
         study_period = int(input("Please enter a valid study period: "))
         print()
+
     if course in study_periods[study_period]:
         print("✓ Course is available in study period", study_period)
         print()
         return True
+
     else:
         print("✕ Course is not available in study period", study_period)
         return False
 
 def Enrollments():
-    print()
+
+    print() # prints a blank line
     userInput = input("Enter the course code: ") # user input is the course code asked for
 
     if userInput.islower(): # if the user input is in lower case, it will be converted to upper case as per the course code format
@@ -117,7 +123,7 @@ def Enrollments():
             if checkPreRequisites(userInput) and checkCoRequisites(userInput) and checkStudyPeriod(userInput):
                 coursesToEnroll.append(userInput)
                 for i in tqdm (range (101),
-                    desc="Loading…",
+                    desc="Adding…",
                     ascii=False, ncols=75):
                     time.sleep(0.01)
                 print("✓ ", userInput,  "added to the list")
@@ -144,9 +150,8 @@ print("=================================")
 
 for course in coursesToEnroll:
     number = 1
-    while number < len(coursesToEnroll):
-        print(number,"->", course)
-        number += 1
+    print(number,"->", course)
+    number += 1
 
 print("=================================")
 
@@ -154,4 +159,4 @@ print()
 print("Good Luck!")
 print()
 
-input("PressT Enter to close!")
+input("Press enter to close!")
