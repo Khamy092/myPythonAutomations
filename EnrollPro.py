31d2523e00dcbe0caa36fe5d8e3ca92e8be8c0aa
khamy092 <khamy092@mymail.unisa.edu.au>
2023-01-16 18:32:51 +1030

    functions are completed. validation in process

--- EnrollPro.py
+++ EnrollPro.py
@@ -4,9 +4,8 @@
 # Date: 15/01/2022
 # Author: Taqi Khaliqdad
 
-
-# function starts here
-
+from tqdm import tqdm
+import time
 # list of all the courses I need to enroll in:
 
 courseCodes = ['INFT 1016', 'COMP 1039', 'INFT 1012', 'INFT 1030', 'INFS 1025', 'INFS 1026', 'INFT 1031',
@@ -16,35 +15,123 @@ courseCodes = ['INFT 1016', 'COMP 1039', 'INFT 1012', 'INFT 1030', 'INFS 1025', 
 
 # list of courses I have already enrolled & passed:
 
-enrolledCourses = [(courseCodes[0], courseCodes[1], courseCodes[2], courseCodes[3], courseCodes[4], courseCodes[5], courseCodes[6],)]
+passedCourses = ['INFT 1016', 'COMP 1039', 'INFT 1012', 'INFT 1030', 'INFS 1025', 'INFS 1026', 'INFT 1031']
 
-# Course Pre-requisites:
-
-INFT1016, COMP1039, INFT1012, INFT1030, INFS1025, INFS1026, INFT1031 = None
-COMP1046 = [COMP1039] ; INFS2044 = [INFT1031, COMP1039]
-INFS2045 = [INFT1031] ; INFS3090 = [INFT1012, INFT1016] ; INFS2041 = [INFS2045]
-INFS2043 = [INFS2045] ; INFS4020 = [INFS1025]
-
-
+# courses with pre-requisites:
+preRequisites = {
+        "COMP 1046": "COMP 1039",
+        "INFS 2044": "INFT 1031",
+        "INFS 2045": "INFT 1031" and "COMP 1039",
+        "INFS 3090": "INFT 1012" and "INFT 1016",
+        "INFS 2041": "INFS 2045",
+        "INFS 2043": "INFS 2045",
+        "INFS 4020": "INFS 1025",
+        "INFT 3033": "COMP 1046",
+        "COMP 2035": "COMP 1046",
+        "COMP 2012": "COMP 1046",
+        "INFS 2011": "INFS 1025",
+        "INFT 2064": "INFS 1025" and "COMP 1012",
+        "COMP 3023": "COMP 2012",
+        "COMP 2019": "COMP1046",
+        "INFT 3043": "INFS 1025" and "COMP 2012",
+        "ICTPROJECT": 72
+    }
 
 # Course Co-requisites:
-courseCoReq = []
-CourseA = INFS2045 = [INFS2044]
-CourseB = INFS2043 = [INFS2041]
+coRequisites = {
+        "INFS 2045": "INFS 2044",
+        "INFS 2043": "INFS 2041",
+        "INFT 2064": "COMP 2012"
+}
 
-studyPeriods = [2, 5]
+# Course study periods:
+study_periods = {
+    2: ['INFT 1016', 'COMP 1039', 'INFT 1012', 'INFT 1030', 'INFS 1025', 'INFS 1026', 'INFT 1031',
+            'COMP 1046', 'INFS 2044', 'INFS 2045', 'INFS 3090', 'INFS 4020', 'COMP 2035', 'COMP 2012',
+            'INFS 2011', 'ELECTIVE', 'COMP 3023', 'ICT PROJECT'],
+    
+    5: ['INFS 2041', 'INFS 2043', 'INFT 3033', 'INFT 2064', 'COMP 2019', 'INFT 3043']
+}
 
+# courses to enroll in:
 coursesToEnroll = []
 
-def findCoursesToEnroll():
+# function starts here
 
-    global coursesToEnroll
-    
-    for course in courseCodes:
-        if course not in enrolledCourses:
-            if course in courseCoReq:
-                if course is None or course in enrolledCourses:
-                    if course in studyPeriods:
-                        coursesToEnroll.append(course)
-                else:
-                    print("You need to enroll in the pre-requisite course first")
+def checkPreRequisites(course): # function to check if the pre-requisites are met
+    if course in preRequisites:
+        if preRequisites[course] in passedCourses:
+            print("✓ Pre-requisite met")
+            return True
+        else:
+            print("✕ Pre-requisite not met")
+            return False
+
+def checkCoRequisites(course): # function to check if the co-requisites are met
+    if course in coRequisites:
+        if coRequisites[course] in coursesToEnroll:
+            print("✓ Co-requisite met")
+            return True
+        else:
+            print("✕ Co-requisite not met")
+            return False
+    else:
+        return True
+
+def checkStudyPeriod(course): # function to check if the course is available in the study period
+    study_period = int(input("Enter the study period: "))
+    if course in study_periods[study_period]:
+        print("✓ Course is available in study period", study_period)
+        return True
+    else:
+        print("✕ Course is not available in study period", study_period)
+        return False
+
+def Enrollments():
+    userInput = input("Enter the course code: ") # user input is the course code asked for
+
+    if userInput.islower(): # if the user input is in lower case, it will be converted to upper case as per the course code format
+        userInput = userInput.upper()
+
+    while userInput not in courseCodes:
+        print("Course not found")
+        userInput = input("Please enter a valid course code: ")
+    if userInput in courseCodes:
+        if userInput in passedCourses:
+            print("You have already passed ", userInput)
+        else:
+            if checkPreRequisites(userInput) and checkCoRequisites(userInput) and checkStudyPeriod(userInput):
+                coursesToEnroll.append(userInput)
+                for i in tqdm (range (101),
+                    desc="Loading…",
+                    ascii=False, ncols=75):
+                    time.sleep(0.01)
+                print("✓ ", userInput,  "added to the list")
+            else:
+                print("✕ Course not added to the list")
+
+
+userExits = False
+while userExits != True:
+
+    Enrollments()
+
+    userExits = input("Do you want to continue? (Y/N): ")
+    while userExits != "Y" and userExits != "N" and userExits != "y" and userExits != "n":
+        userExits = input("Please enter Y or N: ")
+
+    if userExits == "N" or userExits == "n":
+        userExits = True
+        pass
+
+print("Enroll in the following courses: ")
+print("=================================")
+for course in coursesToEnroll:
+    number = 0
+    print(number,"->", course)
+    number += 1
+
+print("=================================")
+print("Good Luck!")
+print()
+input("PressT Enter to close!")
