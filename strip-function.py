684afab694231c2a402c37483bd943fe889dee28
khamy092 <khamy092@mymail.unisa.edu.au>
2023-01-23 15:21:40 +1030

    an example

---
+++ strip-function.py
@@ -0,0 +1,5 @@
+# Description: This program demonstrates the strip function 
+# strip function removes leading and trailing spaces from a string
+
+myString = " Hello World "
+print(myString.strip(), "is the string without leading or trailing spaces")
