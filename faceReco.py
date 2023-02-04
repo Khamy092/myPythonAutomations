2aa53f641bdd847a4c645ef472cc0c93dd513600
khamy092 <khamy092@mymail.unisa.edu.au>
2023-02-04 14:03:54 +1030

    images deleted.

--- faceReco.py
+++ faceReco.py
@@ -2,21 +2,25 @@ import cv2
 import numpy as np
 
 face_names = {
-    "face_7.jpg": "James Maddison",
+    "face_68.jpg": "Taqi",
 }
 # Load the cascade classifier
 face_cascade = cv2.CascadeClassifier("facemodel.xml")
 
 # Load the reference face
-reference_face = cv2.imread("face_7.jpg", cv2.IMREAD_GRAYSCALE) # face_7.jpg is the reference face
+reference_face = cv2.imread("face_5.jpg", cv2.IMREAD_GRAYSCALE) # face_7.jpg is the reference face
 # Read the input image
-img = cv2.imread("image.jpg")
+img = cv2.imread("image1.jpg")
 
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the input image
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+# save the faces into files 
+# for (x, y, w, h) in faces:
+#     face = gray[y:y + h, x:x + w]
+#     cv2.imwrite("face_{}.jpg".format(np.random.randint(100)), face)
 
 # Loop over the faces and compare each face with the reference face
 for (x, y, w, h) in faces:
@@ -27,8 +31,8 @@ for (x, y, w, h) in faces:
         # Draw a rectangle around the face
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         # Add text to indicate that the face was found
-        if "face_7.jpg" in face_names:
-            cv2.putText(img, face_names["face_7.jpg"], (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        if "face_68.jpg" in face_names:
+            cv2.putText(img, face_names["face_68.jpg"], (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 # Display the output
 cv2.imshow("Faces found", img)
